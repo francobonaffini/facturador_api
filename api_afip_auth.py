37aa91eccf22c0ce5eb52e03800a56d8d3595e0b
unknown <codigofranco1@gmail.com>
2024-05-20 17:04:23 -0300

    se agrego la funcion ultima factura autorizada para ver el ultimo comprobante y sumar uno al nuevo comprobante.

--- api_afip_auth.py
+++ api_afip_auth.py
@@ -51,6 +51,167 @@ clave_privada = os.path.join(base,entorno,"privada_facturacion.key")
 url2 = "https://wswhomo.afip.gov.ar/wsfev1/service.asmx?WSDL"
 
 
+def ultimo_autorizado():
+    # URL del servicio web (endpoint) 
+    # PRODUCCION (WSAAS)
+    # url = 'https://wsaa.afip.gov.ar/ws/services/LoginCms?WSDL'
+
+    # DESARROLLO (WSAAS)
+    url = 'https://wsaahomo.afip.gov.ar/ws/services/LoginCms?WSDL'
+
+    #Modificamos el archivo MiLoginTicketRequest.xml para introducirle la hora y fecha actual y sumarle 1 HR:
+    tree = ET.parse(MiLoginTicketRequest1)
+    
+    root = tree.getroot()
+
+    # Obtener el elemento 'generationTime' y actualizar su contenido
+    expiration_time_element = root.find(".//expirationTime")
+    current_time = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+
+    #Script que se ejecuta cuando la hora de expiracion es menor que la hora actual
+    # Dentro de la condicional es: expiration_time_element.text < current_time 
+    if (expiration_time_element.text < current_time ):
+
+        print("La hora de expiracion es menor que la hora actual ejecuta codigo de actualizacion de token")
+        # Calcular la hora de expiración
+    
+        generation_time = datetime.strptime(current_time, '%Y-%m-%dT%H:%M:%S')
+        expiration_time = generation_time + timedelta(hours=1)
+        expiration_time_str = expiration_time.strftime('%Y-%m-%dT%H:%M:%S')
+
+        expiration_time_element = root.find(".//expirationTime")
+        expiration_time_element.text = expiration_time_str
+
+        # Obtén la etiqueta <generationTime>
+        generation_time_element = root.find(".//generationTime")
+
+        # Obtiene la fecha y hora actual
+        current_time = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+
+        # Actualiza el contenido de la etiqueta <generationTime>
+        generation_time_element.text = current_time
+        # Guardar los cambios de vuelta al archivo
+        tree.write(MiLoginTicketRequest1)
+
+        # Contenido del CMS que deseas enviar
+        # Aquí debes poner el contenido del CMS en formato base64
+
+        command = [
+            'openssl', 'cms', '-sign',
+            '-in', MiLoginTicketRequest1,
+            '-out', MiLoginTicketRequest2,
+            '-signer', certificado_pem,
+            '-inkey', clave_privada,
+            '-nodetach', '-outform', 'PEM'
+        ]
+
+        try:
+            subprocess.run(command, check=True)
+            print("Comando OpenSSL ejecutado exitosamente.")
+        except subprocess.CalledProcessError as e:
+            print("Error al ejecutar el comando OpenSSL:", e)
+
+        # Ya tenemos el "MiLoginTicketRequest2" producto del comando anterior.
+
+        # FUNCION para el contenido del archivo y limpia los ---BEGIN y END CMS---
+        def read_cms_file(file_path):
+            with open(file_path, 'r') as file:
+                lines = file.readlines()
+            
+            content = ''
+            inside_cms = False
+            
+            for line in lines:
+
+                if line.strip() == '-----BEGIN CMS-----':
+                    inside_cms = True
+                    continue
+
+                elif line.strip() == '-----END CMS-----':
+                    inside_cms = False
+                    continue
+                
+                if inside_cms:
+                    content += line
+            
+            return content
+
+        #Aqui tenemos el CMS formateado sin el --BEGIN-- ni --END--
+        cms_content = read_cms_file(MiLoginTicketRequest2)
+        
+        # Crear un cliente SOAP
+        client = zeep.Client(url)
+
+        try:
+            response = client.service.loginCms(cms_content)
+        
+        except zeep.exceptions.Fault as e:
+            print(f"Error en la respuesta: {e}")
+
+        root = ET.fromstring(response)
+
+        token = root.find('.//token').text
+        sign = root.find('.//sign').text
+
+        #Grabamos los archivos .TXT para que se mantengan en el disco local para cada peticion lo tengamos en el disco duro 
+
+        nombre_archivo1 = os.path.join(base,"token.txt")
+
+        with open(nombre_archivo1, 'w') as archivo:
+            # Escribe el contenido en el archivo
+            archivo.write(token)
+
+        nombre_archivo2 = os.path.join(base,"sign.txt")
+
+        with open(nombre_archivo2, 'w') as archivo:
+            # Escribe el contenido en el archivo
+            archivo.write(sign)
+
+    else:
+
+        # El tiempo de expiracion no está expirado, solo toma el token y el sign y cre las variables para utilizar luego en el WSN.
+
+        nombre_archivo1 = os.path.join(base,"token.txt")
+        with open(nombre_archivo1, 'r') as archivo:
+            # Lectura del contenido
+            token = archivo.read()
+
+        nombre_archivo2 = os.path.join(base,"sign.txt")
+        with open(nombre_archivo2, 'r') as archivo:
+            # Lectura del contenido
+            sign = archivo.read()
+
+    # # # # # # W S N # # # # # # 
+
+    client = Client(url2, transport=transport)
+    
+    cuitRepresentada = 20375182905  # Reemplaza esto con el valor real del CUIT representado
+
+    # Ponemos las variables que irán en la consulta.
+
+    # Llamar a la operación getPersona del servicio web
+    # try:
+   
+    # Crear el objeto AuthRequest
+    Auth = client.get_type('ns0:FEAuthRequest')
+
+    auth = Auth(
+        Token=token,
+        Sign=sign,
+        Cuit=cuitRepresentada
+    )
+
+    # Llamar a la operación FECAESolicitar
+    response = client.service.FECompUltimoAutorizado(
+        Auth=auth,
+        PtoVta = 1, # tiene que tener WS habilitado.
+        CbteTipo = 6 # Facturas A (las C, son 11)
+    )
+    
+    print(response['CbteNro'])
+
+    return response['CbteNro']
+
 def facturador_lotes():
 
     # URL del servicio web (endpoint) 
@@ -203,22 +364,23 @@ def facturador_lotes():
         Cuit=cuitRepresentada
     )
 
+    utlimo_comprobante = ultimo_autorizado()
 
     # Datos de la factura. (podrían ser varios, acá solo hay que iterarlas)
     facturas = [
         {
             "Concepto": 1,
             "DocTipo": 80,
-            "DocNro": 20375182906,
-            "CbteDesde": 3,
-            "CbteHasta": 3,
+            "DocNro": 23389116394,
+            "CbteDesde": utlimo_comprobante +1,
+            "CbteHasta": utlimo_comprobante +1,
             "CbteFch": datetime.now().strftime("%Y%m%d"),
             "ImpTotal": 121.0,
             "ImpTotConc": 0.0,
             "ImpNeto": 100.0,
             "ImpOpEx": 0.0,
             "ImpTrib": 0.0,
-            "ImpIVA": 21,
+            "ImpIVA": 21.0,
             "FchServDesde": '',
             "FchServHasta": '',
             "FchVtoPago": '',
@@ -227,22 +389,22 @@ def facturador_lotes():
             "Iva": [
                 {
                     "Id": 5,  # 21%
-                    "BaseImp": 100.0,
-                    "Importe": 21.0
+                    "BaseImp": 100.00,
+                    "Importe": 21.00
                 }
             ]
         },
         # Añade más facturas según sea necesario
     ]
 
-    # Crear el objeto FeCabReq
+    # Crear el objeto FeCabReq TIPO DE COMPROBANTE Y PTO DE VENTA (FACT C = 11, PTO.VTA = 1)
     FeCabReq = client.get_type('ns0:FECAECabRequest')
     fe_cab_req = FeCabReq(
         CantReg=len(facturas),
         PtoVta=1,
-        CbteTipo=1
+        CbteTipo=6
     )
-
+    
     # Crear los objetos FECAEDetRequest
     FeDetReq = client.get_type('ns0:ArrayOfFECAEDetRequest')
     FECAEDetRequest = client.get_type('ns0:FECAEDetRequest')
@@ -297,7 +459,6 @@ def facturador_lotes():
     
     print(response)
 
-
 def generar_factura_pdf(datos_factura, logo_path, output_path):
     c = canvas.Canvas(output_path, pagesize=A4)
     ancho, alto = A4
@@ -310,6 +471,12 @@ def generar_factura_pdf(datos_factura, logo_path, output_path):
     c.setFillColor(colors.white)
     c.setFont("Helvetica-Bold", 16)
     c.drawString(2 * cm, alto - 1.8 * cm, "Factura")
+    c.setFont("Helvetica-Bold", 26)
+    c.drawString(10 * cm, alto - 1.8 * cm, "B")
+    c.setFont("Helvetica", 9)
+    c.drawString(10 * cm, alto - 2.2 * cm, "006")
+
+
 
     # Agregar el logo de la empresa
     logo = Image.open(logo_path)
@@ -317,8 +484,12 @@ def generar_factura_pdf(datos_factura, logo_path, output_path):
     logo_ratio = logo_width / logo_height
     logo_display_width = 3 * cm
     logo_display_height = logo_display_width / logo_ratio
-    c.drawImage(logo_path, x=ancho - 4 * cm, y=alto - .5 * cm - logo_display_height, width=logo_display_width, height=logo_display_height)
+    c.drawImage(logo_path, x=ancho - 5 * cm, y=alto - .5 * cm - logo_display_height, width=logo_display_width, height=logo_display_height)
 
+    # Agregar una línea recta horizontal
+    c.setStrokeColor(color_azul)
+    c.setLineWidth(1)  # Ancho de la línea
+    c.line(2 * cm, alto - 7 * cm, ancho - 2 * cm, alto - 7 * cm)
 
     # Datos de la empresa
     c.setFillColor(colors.white)
@@ -346,6 +517,8 @@ def generar_factura_pdf(datos_factura, logo_path, output_path):
     c.drawString(2 * cm, alto - 5.5 * cm, f"N°CAE: {datos_factura['factura_nro']}")
 
     # Detalles de la factura
+    c.setFillColor(colors.HexColor("#F2F2F2"))
+    c.rect(0, alto - 8.2 * cm, ancho, 0.7 * cm, fill=True, stroke=False)
     c.setFont("Helvetica-Bold", 10)
     c.setFillColor(color_azul)
     c.drawString(2 * cm, alto - 8 * cm, "Descripción")
@@ -353,7 +526,6 @@ def generar_factura_pdf(datos_factura, logo_path, output_path):
     c.drawString(15 * cm, alto - 8 * cm, "Precio Unitario")
     c.drawString(18 * cm, alto - 8 * cm, "Total")
 
-    c.setFillColor(colors.black)
     c.setFont("Helvetica", 10)
     y = alto - 9 * cm
     for item in datos_factura['items']:
@@ -379,7 +551,7 @@ def generar_factura_pdf(datos_factura, logo_path, output_path):
     c.showPage()
     c.save()
 
-# Datos de ejemplo
+# Datos de ejemplo para crear el PDF
 datos_factura = {
     "cliente_nombre": "Juan Pérez",
     "cliente_direccion": "Calle Falsa 123",
@@ -401,7 +573,14 @@ datos_factura = {
 }
 
 
+
+
 # Generar la factura
+
+# Ejecucion de funciones: >
+
 # generar_factura_pdf(datos_factura, "logo.png", "factura.pdf")
 
-facturador_lotes()
+# facturador_lotes()
+
+# ultimo_autorizado()
